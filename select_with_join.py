d9e12d08d79054735bf71eb851a0eab5c27c7b2f
Salutah <salutahmoha@gmail.com>
2025-03-17 13:53:04 +0300

    creating select_with_join.py file to select both tables in a joint form

--- select_with_join.py
+++ select_with_join.py
@@ -1,15 +1,15 @@
-# from main import session
-# from models import Course, Student
-# from sqlalchemy import select
+from main import session
+from models import Course, Student
+from sqlalchemy import select
 
-# joining = select(Student).join(Student.course).where (
-#     Course.coursename == 'python'
-# ).where(
-#     Student.studentname == 'Brian'
-# )
+joining = select(Student).join(Student.course).where (
+    Course.coursename == 'python'
+).where(
+    Student.studentname == 'Brian'
+)
 
-# result = session.scalars(joining).all()
+result = session.scalars(joining).all()
 
-# print(result)
+print(result)
 
-# # select studentname, coursename from students INNER JOIN courses ON courses.course_id = students.course_id
+# select studentname, coursename from students INNER JOIN courses ON courses.course_id = students.course_id

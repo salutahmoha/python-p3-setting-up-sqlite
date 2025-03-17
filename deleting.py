4852d9f097f82f0d39e0de12d8071d30da282cef
Salutah <salutahmoha@gmail.com>
2025-03-17 13:54:30 +0300

    deleting specific value from table

--- deleting.py
+++ deleting.py
@@ -1,7 +1,7 @@
-# from main import session
-# from models import Course, Student
+from main import session
+from models import Course, Student
 
-# course = session.query(Course).filter_by(id=6).first()
-# session.delete(course)
+course = session.query(Course).filter_by(id=6).first()
+session.delete(course)
 
-# session.commit()
+session.commit()
